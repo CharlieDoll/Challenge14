c7c74eab5656e7d0c09519ca763a7ab0ae266dee
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-01-12 15:32:21 +0000

    login code

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Comment, Post } = require("../../models");
 // TODO: Import the custom middleware
 
-// GET all galleries for homepage
+// GET all posts for homepage
 router.get("/", async (req, res) => {
   console.log("hello");
   try {
@@ -27,7 +27,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-// GET one gallery
 // TODO: Replace the logic below with the custom middleware
 router.get("/post/:id", async (req, res) => {
   // If the user is not logged in, redirect the user to the login page
@@ -36,7 +35,7 @@ router.get("/post/:id", async (req, res) => {
   } else {
     // If the user is logged in, allow them to view the gallery
     try {
-      const bPost = await Post.findByPk(req.params.id, {
+      const dbPost = await Post.findByPk(req.params.id, {
         include: [
           {
             model: Comment,
@@ -44,29 +43,8 @@ router.get("/post/:id", async (req, res) => {
           },
         ],
       });
-      const blogPost = dbBlogData.get({ plain: true });
-      res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  }
-});
-
-// GET one painting
-// TODO: Replace the logic below with the custom middleware
-router.get("/painting/:id", async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect("/login");
-  } else {
-    // If the user is logged in, allow them to view the painting
-    try {
-      const dbPaintingData = await Painting.findByPk(req.params.id);
-
-      const painting = dbPaintingData.get({ plain: true });
-
-      res.render("painting", { painting, loggedIn: req.session.loggedIn });
+      const blogPost = dbPost.get({ plain: true });
+      res.render("post", { blogPost, loggedIn: req.session.loggedIn });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -83,4 +61,13 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// dashboard route
+
+router.get("/dashboard", (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+  } else {
+    res.render("dashboard");
+  }
+});
 module.exports = router;
